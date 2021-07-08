d549550ed206e8b60723a8dc719cd7984eca4b83
Sergey Volodin <whiteigel@gmail.com>
2021-07-08 13:25:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+import requests
+import time
+import datetime
+
+
+def get_news(keyword, fromdate, todate):
+    news = {}
+    keyword = keyword
+    fromdate = int(time.mktime(datetime.datetime.strptime(fromdate, "%d/%m/%Y").timetuple()))
+    todate = int(time.mktime(datetime.datetime.strptime(todate, "%d/%m/%Y").timetuple()))
+    url = f"https://api.stackexchange.com/2.3/questions?fromdate={fromdate}&todate={todate}&order=asc&sort=activity&tagged={keyword}&site=stackoverflow"
+    response = requests.get(url=url)
+    res = response.json()['items']
+    if response.status_code != 200:
+        print("ERROR")
+    for ind, elm in enumerate(res):
+        title = elm['title']
+        q_link = elm['link']
+        news[title] = q_link
+    return news
+
+if __name__ == '__main__':
+    pprint(get_news('Python', '06/07/2021', '08/07/2021'))
+
+
